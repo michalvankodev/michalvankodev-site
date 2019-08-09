f37a1d4ec7f100ef61e3d200c97523d6c874a840
Michal Vanko <mklakakilli@gmail.com>
2019-08-09 18:34:03 +0200

    Added Projects into portfolio

---
+++ src/components/portfolio/skills.svelte
@@ -0,0 +1,17 @@
+<section id="skills">
+  <h3>Skills</h3>
+  <p>Slovak is my mother tongue and I've learned English as my second language. I speak English on advanced level.</p>
+  <p>
+    I'm an experienced <em>Linux Desktop</em> user. I prefer to use open source libraries and technologies while I develop solutions.
+    <br>
+    I'm in good command of Office Tools and I've experience with image manipulation programs like <em>GIMP</em> and <em>Inkscape</em>.
+    <br>
+    I can also compose music and sounds in <em>digital audio workstation</em>.
+  </p>
+  <p>I'm passionate about <em>software architecture</em>. My goal is to be able to design suitable solution for any kind of product. From small <em>presentation sites</em>, <em>IOT devices</em>, to large <em>enterprise applications running on cloud</em>.</p>
+  <p>
+    I do <em>public speaking</em> and I am not afraid to share my knowledge and passion about technology.
+  </p>
+  <p>I'm advanced user of source code management tools <em>git</em> and <em>svn</em>.</p>
+  <p>I've a driving licence for category B &#128663;.</p>
+</section>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,3 +1,10 @@
+<script>
+	import Personal from '../components/portfolio/personal.svelte';
+  import Skills from '../components/portfolio/skills.svelte';
+  import Workhistory from '../components/portfolio/workhistory.svelte';
+  import Projects from '../components/portfolio/projects.svelte';
+</script>
+
 <svelte:head>
 	<title>Portfolio - Michal Vanko</title>
 </svelte:head>
@@ -6,87 +13,7 @@
 
 <h2>Software Developer</h2>
 
-<section id="personal">
-  <h3>Personal Information</h3>
-	<p>I was born on 26th of May in Košice, Slovakia. I still live here which makes me Slovak.</p>
-  <h4>Hobbies:</h4>
-	<p>
-    I enjoy playing basketball with my friends. I also like to play other team sports like football and hockey.
-    I also play squash and table tennis. Once I've won a competition in squash at my university.
-    During summer I love water skiing and swimming in a nearby lake.
-    <br />
-    I am very passionate about music. I've also tried some software for composing music but I am not really hooked into that yet.
-    From time to time I enjoy playing board games with my friends.
-  </p>
-	<h4>Interests:</h4>
-	<p>
-		I like to explore new technologies and I'm passionate about <em>Open Source movement</em>,
-		<em>Internet of Things</em> applications and <em>Linux desktop evolution</em>.
-		<br />
-		I am interested in modern software architecture and <em>reactive programming</em>.
-		<br />
-		I've attended various <strong>tech conferences and hackathons</strong>. I like them for all of the fascinating ideas that might be invented.
-		<br />
-		I've given presentations on various topics related to <em>web development</em>. You can <a href='#presentations'>take a look at some of them here</a>.
-		<br />
-    I enjoy <strong>teaching and explaining</strong> how various technologies and techniques work to my colleagues for their better understanding.
-		<br />
-    I take advantage of <strong>test driven development</strong>.
-	</p>
-</section><!--/personal-->
-
-<section id="skills">
-  <h3>Skills</h3>
-  <p>Slovak is my mother tongue and I've learned English as my second language. I speak English on advanced level.</p>
-  <p>
-    I'm an experienced <em>Linux Desktop</em> user. I prefer to use open source libraries and technologies while I develop solutions.
-    <br>
-    I'm in good command of Office Tools and I've experience with image manipulation programs like <em>GIMP</em> and <em>Inkscape</em>.
-    <br>
-    I can also compose music and sounds in <em>digital audio workstation</em>.
-  </p>
-  <p>I'm passionate about <em>software architecture</em>. My goal is to be able to design suitable solution for any kind of product. From small <em>presentation sites</em>, <em>IOT devices</em>, to large <em>enterprise applications running on cloud</em>.</p>
-  <p>
-    I do <em>public speaking</em> and I am not afraid to share my knowledge and passion about technology.
-  </p>
-  <p>I'm advanced user of source code management tools <em>git</em> and <em>svn</em>.</p>
-  <p>I've a driving licence for category B &#128663;.</p>
-</section><!--/skills-->
-
- <section id="workhistory">
-  <h3>Work Experience</h3>
-  <p>
-    I am doing web development for approximately 14 years.
-    I've started at very young age of 14.
-    My very first web site was for promotion of my own Counter-Strike clan.
-    Then I had an opportunity to create a important registration system
-    for marathon runners for Europe's oldest marathon event.
-    That basically started my career as a web developer. I had some projects while I was studying on high school and university.
-    After that I started to work full time as a web developer and gain more experience in developing real-time web applications.
-  </p>
-
-  <article>
-    <h4>localhost.company s.r.o</h4>
-    <p>localhost.company is a small company which has only around 40 developers and most of them are students.
-      I work in <em>localhost</em> as a <strong>Senior front end developer</strong>. I am focused on delivering a high quality product and to teach and help other colleagues.
-    </p>
-    <p>I've started to work there in October 2018.</p>
-    <span class="location"><em>localhost.company s.r.o</em> Boženy Nemcovej 30, 040 01 Košice, Slovakia</span>
-  </article>
-
-  <article>
-    <h4>Ness KE s.r.o</h4>
-    <p>I worked in Ness as a <strong>Senior front end developer</strong>. In Ness, I've been able to work for different clients on various projects.</p>
-    <p>I joined Ness as a Junior front end developer and I've been able to quickly proceed to Senior level with my engagement</p>
-    <p>I started to work in March 2013 and left in October 2018.</p>
-    <span class="location"><em>Ness KE s.r.o.</em>Moldavská cesta 10/B, 040 11 Košice, Slovakia</span>
-  </article>
-  <article>
-    <h4>eSOLUTIONS s.r.o.</h4>
-    <p>I worked in eSOLUTIONS as a <strong>extern web developer</strong></p>
-    <p>I was building front-end and back-end for websites and web applications. Mostly using <em>PHP</em>, <em>MySQL</em>, <em>JavaScript</em>, <em>HTML</em>, <em>CSS</em></p>
-    <p>I started to work in 2007 and stopped in 2012.</p>
-    <span class="location"><em>eSOLUTIONS s.r.o.</em> Hroncová 2, Košice, Slovakia</span>
-  </article>
-</section>
-
+<Personal />
+<Skills />
+<Workhistory />
+<Projects />
