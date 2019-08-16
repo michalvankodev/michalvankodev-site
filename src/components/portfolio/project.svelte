9d06c9833390430ee06c60819b21bcc40316eb4d
Michal Vanko <mklakakilli@gmail.com>
2019-08-16 20:26:46 +0200

    Load projects from cms in portfolio

---
+++ src/components/portfolio/project.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let project
+</script>
+
+<article class="project">
+  <h3>{project.name}</h3>
+  <section class="description">
+    {@html project.description}
+  </section>
+  {#if project.image}
+    <aside>
+      <img src="{project.image}" class="project-image" alt="{project.image.description}" />
+    </aside>
+  {/if}
+  <aside>
+  </aside>
+</article>
