01fd2a7fe3bcd716edb226a992c2b66257aa4a84
Michal Vanko <mklakakilli@gmail.com>
2019-09-10 21:23:50 +0200

    Add some dark styles

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <style>
-	nav {
+	/* nav {
 		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
@@ -15,7 +15,7 @@
 	}
 
 	/* clearfix */
-	ul::after {
+	/* ul::after {
 		content: '';
 		display: block;
 		clear: both;
@@ -39,22 +39,48 @@
 		background-color: rgb(255,62,0);
 		display: block;
 		bottom: -1px;
-	}
+	} */
 
-	a {
+	/* a {
 		text-decoration: none;
 		padding: 1em 0.5em;
 		display: block;
-	}
+  } */
+  
+  nav {
+    position: sticky;
+    top: 0;
+    background: rgb(31, 22, 94);
+    z-index: 1;
+    padding: 0.5em;
+    box-shadow: 0px 2px 4px #090808;
+  }
+
+  nav a {
+    padding: 0.4em 0.5em;
+    text-decoration: none;
+  }
+
+  nav a:hover {
+    text-decoration: underline
+  }
 </style>
 
 <nav>
-	<ul>
+	<!-- <ul> -->
 		<!-- <li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li> -->
-		<li><a class='{segment === "portfolio" ? "selected" : ""}' href='portfolio'>portfolio</a></li>
+		<!-- <li><a class='{segment === "portfolio" ? "selected" : ""}' href='portfolio'>portfolio</a></li> -->
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<!-- <li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li> -->
-	</ul>
+	<!-- </ul> -->
+
+  <div class="page-navigation">
+	  <a href="portfolio#personal-information">About</a>
+		<a href="portfolio#skills">Skills</a>
+		<a href="portfolio#work-history">Work History</a>
+		<a href="portfolio#projects">Projects</a>
+		<a href="portfolio#education">Education</a>
+  </div>
 </nav>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,18 +5,30 @@
 </script>
 
 <style>
+  .app-content {
+		background: linear-gradient(176deg, rgb(31, 22, 94) 0%,rgb(67, 117, 236) 100%);
+    background-attachment: fixed;
+		color:#f2f0f0;
+  }
+
 	main {
 		position: relative;
 		max-width: 56em;
-		background-color: white;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
+
+  :global(::selection) {
+    background: #0dd0d0;
+  }
+
 </style>
 
-<Nav {segment}/>
+<div class="app-content">
+  <Nav {segment}/>
 
-<main>
-	<slot></slot>
-</main>
+  <main>
+    <slot></slot>
+  </main>
+</div>
