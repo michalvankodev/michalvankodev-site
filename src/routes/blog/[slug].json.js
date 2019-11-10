cb59978ad8fdad1227f3b084c3ff9bcc1c750c98
Michal Vanko <mklakakilli@gmail.com>
2019-11-10 21:37:50 +0100

    Make blog post index listing

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -1,28 +1,30 @@
-import posts from './_posts.js';
+import { readFile } from 'fs'
+import { promisify } from 'util'
+import fm from 'front-matter'
+import { parseField } from '../../markdown/parse-markdown'
 
-const lookup = new Map();
-posts.forEach(post => {
-	lookup.set(post.slug, JSON.stringify(post));
-});
+export async function get(req, res, next) {
+  // the `slug` parameter is available because
+  // this file is called [slug].json.js
+  const { slug } = req.params
 
-export function get(req, res, next) {
-	// the `slug` parameter is available because
-	// this file is called [slug].json.js
-	const { slug } = req.params;
+  let postSource
+  try {
+    postSource = await promisify(readFile)(`_posts/blog/${slug}.md`, 'utf-8')
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      res.statusCode = 404
+      res.end('Post not found \n' + e.toString())
+      return
+    }
+    res.statusCode = 500
+    res.end('Error loading post source file. \n' + e.toString())
+    return
+  }
 
-	if (lookup.has(slug)) {
-		res.writeHead(200, {
-			'Content-Type': 'application/json'
-		});
+  const parsedPost = fm(postSource)
+  const response = parseField('body')(parsedPost)
 
-		res.end(lookup.get(slug));
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'application/json'
-		});
-
-		res.end(JSON.stringify({
-			message: `Not found`
-		}));
-	}
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(response))
 }
--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,16 +1,36 @@
-import posts from './_posts.js';
+import { readdir, readFile } from 'fs'
+import { promisify } from 'util'
+import fm from 'front-matter'
+import marked from 'marked'
 
-const contents = JSON.stringify(posts.map(post => {
-	return {
-		title: post.title,
-		slug: post.slug
-	};
-}));
+export async function get(req, res) {
+  const files = await promisify(readdir)(`_posts/blog/`, 'utf-8')
 
-export function get(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'application/json'
-	});
+  const contents = await Promise.all(
+    files.map(async file => {
+      const fileContent = await promisify(readFile)(
+        `_posts/blog/${file}`,
+        'utf-8'
+      )
+      const parsedAttributes = fm(fileContent)
 
-	res.end(contents);
+      const lineOfTextRegExp = /^(?:\w|\[).+/gm
+      const lines = parsedAttributes.body
+        .match(lineOfTextRegExp)
+        .slice(0, 4)
+        .join('\n')
+
+      const preview = marked(lines)
+      return {
+        ...parsedAttributes.attributes,
+        preview,
+      }
+    })
+  )
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  })
+
+  res.end(JSON.stringify(contents))
 }
