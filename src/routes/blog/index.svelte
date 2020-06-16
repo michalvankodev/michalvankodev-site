a6e7d42d845974785e6841e0eaea907fba5142d2
Michal Vanko <mklakakilli@gmail.com>
2020-06-16 15:27:06 +0200

    Add footer to article

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <script>
-  import { format } from 'date-fns'
+  import ArticleFooter from '../../components/blog/article-footer.svelte'
 
   export let posts
   export let query
@@ -31,35 +31,6 @@
 
   .post-list > li:not(:last-child) {
     margin-bottom: 2em;
-  }
-
-  .tags-list {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: inline;
-  }
-
-  .tags-list li {
-    display: inline;
-    font-style: italic;
-  }
-
-  time {
-    font-style: italic;
-  }
-
-  footer {
-    display: flex;
-    font-size: 0.85em;
-    justify-content: space-between;
-    padding-top: 0.2em;
-    margin-top: 0.4em;
-    border-top: 1px solid #c0c1e1;
-  }
-
-  .lighten {
-    color: #595a8f;
   }
 
   .see-all {
@@ -102,28 +73,8 @@
           </h2>
         </header>
         {@html post.preview}
-        <footer>
-          <div class="article-tags">
-            {#if post.tags.length > 0}
-              <span class="lighten">Tags:</span>
-              <ul class="tags-list">
-                {#each post.tags as tag}
-                  <li>
-                    <a href="blog?tag={tag}">{tag}</a>
-                  </li>
-                {/each}
-              </ul>
-            {/if}
-          </div>
-          <div class="created-at">
-            <span class="lighten">Published on</span>
-            <time datetime={post.date}>
-              {format(new Date(post.date), "do MMMM',' y")}
-            </time>
-          </div>
-        </footer>
       </article>
-
+      <ArticleFooter post={post} />
     </li>
   {/each}
 </ul>
