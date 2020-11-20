2205750e3bfaa92f25c7089b2548b6df720ab4e3
Michal Vanko <mklakakilli@gmail.com>
2020-11-20 16:27:23 +0100

    Try it on dev

---
+++ amplify/backend/function/authFunction/src/callback-script.js
@@ -0,0 +1,17 @@
+exports.getScript = function getScript(mess, content) {
+  return `<html><body><script>
+  (function() {
+    function receiveMessage(e) {
+      console.log("receiveMessage %o", e)
+      window.opener.postMessage(
+        'authorization:github:${mess}:${JSON.stringify(content)}',
+        e.origin
+      )
+      window.removeEventListener("message",receiveMessage,false);
+    }
+    window.addEventListener("message", receiveMessage, false)
+    console.log("Sending message: %o", "github")
+    window.opener.postMessage("authorizing:github", "*")
+    })()
+  </script></body></html>`
+}
