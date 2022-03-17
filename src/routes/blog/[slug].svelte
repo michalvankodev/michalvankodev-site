cf487d419c835a57ec25936da7034c68de2bf35c
Michal Vanko <mklakakilli@gmail.com>
2022-03-17 19:10:29 +0100

    Format with prettier

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,6 +1,6 @@
 <script context="module" lang="ts">
   import type { LoadInput, LoadOutput } from '@sveltejs/kit/types/page'
-  import { onMount } from 'svelte';
+  import { onMount } from 'svelte'
 
   export async function load({
     fetch,
@@ -29,12 +29,11 @@
 <script lang="ts">
   import ArticleFooter from '../../components/blog/ArticleFooter.svelte'
   import { contentClass } from './blog.css'
-  import type { PostContent } from './_content';
+  import type { PostContent } from './_content'
 
   export let post: PostContent
   export let loadMermaid = false
 
-  
   onMount(() => {
     // We have to load mermaid after the mount so svelte doesn't rerender the parsed body on mount
     // This way we create the svg diagram after whole page is rendered
@@ -45,7 +44,9 @@
 <svelte:head>
   <title>{post.title}</title>
   {#if loadMermaid}
-    <script id="mermaid-script" src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+    <script
+      id="mermaid-script"
+      src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
     <script>
       document.getElementById('mermaid-script').addEventListener('load', () => {
         mermaid.init()
