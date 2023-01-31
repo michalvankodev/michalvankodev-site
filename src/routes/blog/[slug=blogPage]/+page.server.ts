b3153ae06a1074d94ca48b5ea70fa12e2c69db82
Michal Vanko <mklakakilli@gmail.com>
2023-01-31 20:04:22 +0100

    Blog posts working now

---
+++ src/routes/blog/[slug=blogPage]/+page.server.ts
@@ -0,0 +1,33 @@
+import { readFile } from 'fs'
+import { promisify } from 'util'
+import fm from 'front-matter'
+import { parseField } from '../../../markdown/parse-markdown'
+import { error, json } from '@sveltejs/kit'
+import type { PostAttributes } from '../content'
+import type { PageServerLoad } from './$types'
+
+export interface SinglePost {
+  body: string
+}
+
+export const load = (async ({ params: { slug } }) => {
+  let postSource: string
+  try {
+    postSource = await promisify(readFile)(`_posts/blog/${slug}.md`, 'utf-8')
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    if (e.code === 'ENOENT') {
+      throw error(404, 'Post not found \n' + e.toString())
+    }
+    throw e
+  }
+
+  const parsedPost = fm<PostAttributes>(postSource)
+
+  const post = parseField<SinglePost>('body')({
+    ...parsedPost.attributes,
+    body: parsedPost.body,
+  })
+
+  return post
+}) satisfies PageServerLoad
---
+++ src/routes/blog/[slug=blogPage]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import ArticleFooter from '../../../components/blog/ArticleFooter.svelte'
+  import type { PageData } from './$types'
+  import { contentClass } from './blog.css'
+
+  export let data: PageData
+</script>
+
+<svelte:head>
+  <title>{data.title}</title>
+</svelte:head>
+
+<h1>{data.title}</h1>
+
+<div class="content {contentClass}">
+  {@html data.body}
+</div>
+<ArticleFooter post={data} />
