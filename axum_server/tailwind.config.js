255536c681a304d7518478d20c9aa6d9ab8237ca
Michal Vanko <mklakakilli@gmail.com>
2024-09-12 15:00:20 +0200

    Responsive design for index page

--- axum_server/tailwind.config.js
+++ axum_server/tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./templates/**/*.html"],
+	content: ["./templates/**/**.html"],
 	theme: {
 		extend: {
 			spacing: {
 				note: "60rem",
 				read: "64rem",
 				image: "70rem",
+				maxindex: "100rem",
 			},
 			width: {
 				note: "60rem",
 				read: "64rem",
 				image: "70rem",
+				maxindex: "100rem",
 			},
 		},
 	},
